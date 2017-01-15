1498740e5f3d3a108c1fa34c57e5214f2ecbbb50
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 18:12:35 +1100

    NLC 1i5a - update NLC_PREPROCESSOR_GENERATE_COMMENTS implementation for !isMath (sentences on same line)

--- NLC/NLCpreprocessorMath.cpp
+++ NLC/NLCpreprocessorMath.cpp
@@ -26,7 +26,7 @@
  * File Name: NLCpreprocessorMath.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Natural Language Programming Interface (compiler)
- * Project Version: 1i4c 22-August-2014
+ * Project Version: 1i5a 23-August-2014
  * Requirements: requires text parsed by BAI General Intelligence Algorithm (GIA)
  *
  *******************************************************************************/

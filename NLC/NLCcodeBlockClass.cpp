bc4da3d94bae079b5e692e974d23f50fa5bc0428
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 18:15:13 +1100

    NLC 1n24b - re-enable NLC_SUPPORT_LOGICAL_CONDITION_OPERATIONS_BASED_ON_CONCEPTS_BASIC (partially revert 1n24a)

--- NLC/NLCcodeBlockClass.cpp
+++ NLC/NLCcodeBlockClass.cpp
@@ -26,7 +26,7 @@
  * File Name: NLCcodeBlockClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Natural Language Programming Interface (compiler)
- * Project Version: 1n24a 03-February-2015
+ * Project Version: 1n24b 03-February-2015
  * Requirements: requires text parsed by BAI General Intelligence Algorithm (GIA)
  *
  *******************************************************************************/
@@ -1015,7 +1015,7 @@ bool findIndefiniteEntityCorrespondingToDefiniteEntityInSameContext(vector<GIAen
 		
 		GIAreferenceTraceParameters referenceTraceParameters;
 		referenceTraceParameters.referenceSetID = referenceSetID;
-		#ifdef GIA_SUPPORT_DEFINE_REFERENCE_CONTEXT_BY_TEXT_INDENTATION
+		#ifdef GIA_SUPPORT_NLC_INTEGRATION_DEFINE_REFERENCE_CONTEXT_BY_TEXT_INDENTATION
 		referenceTraceParameters.referenceSetDefiniteEntity = true;	//referenceSetDefiniteEntity
 		//referenceTraceParameters.firstSentenceInList = firstNLCsentenceInList;
 		#endif
--- NLC/NLCpreprocessor.h
+++ NLC/NLCpreprocessor.h
@@ -26,7 +26,7 @@
  * File Name: NLCpreprocessor.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Natural Language Programming Interface (compiler)
- * Project Version: 1n24a 03-February-2015
+ * Project Version: 1n24b 03-February-2015
  * Requirements: requires text parsed by BAI General Intelligence Algorithm (GIA)
  *
  *******************************************************************************/

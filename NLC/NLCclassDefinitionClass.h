c32bb9a080db7cea05bd737553a8f33c5030f180
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 18:15:36 +1100

    NLC 1p1a - NLC_USE_LIBRARY: moveFunction fix; current workaround is not possible as to condition object context will be parsed rather than to condition object being added to from condition object. So therefore must upgrade Stanford Parser or GIA to attach the to condition object correctly to the object entity NLC 1p1a - [OLD] NLC_USE_LIBRARY: moveFunction fix - change "if(removeItemFromVectorList(fromObjectMoveObjectPropertyList, moveObject))" to "if(removeItemFromVectorListByName(fromObjectMoveObjectPropertyList, moveObject->name))" NLC 1p1a - NLC_USE_LIBRARY: moveFunction fix

--- NLC/NLCclassDefinitionClass.h
+++ NLC/NLCclassDefinitionClass.h
@@ -26,7 +26,7 @@
  * File Name: NLCclassDefinitionClass.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Natural Language Programming Interface (compiler)
- * Project Version: 1o7b 10-May-2015
+ * Project Version: 1p1a 06-June-2015
  * Requirements: requires text parsed by BAI General Intelligence Algorithm (GIA)
  *
  *******************************************************************************/

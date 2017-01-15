9043852cb08653bcc6e940dc716993b9ce81959c
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 18:12:05 +1100

    NLC 1g16b - update generateObjectInitialisationsBasedOnPropertiesAndConditions() to detect negative qualities

--- NLC/NLCtranslatorCodeBlocksOperations.cpp
+++ NLC/NLCtranslatorCodeBlocksOperations.cpp
@@ -26,7 +26,7 @@
  * File Name: NLCtranslatorCodeBlocksOperations.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Natural Language Programming Interface (compiler)
- * Project Version: 1g16a 17-July-2014
+ * Project Version: 1g16b 18-July-2014
  * Requirements: requires text parsed by BAI General Intelligence Algorithm (GIA)
  *
  *******************************************************************************/
@@ -750,7 +750,7 @@ bool generateObjectInitialisationsBasedOnPropertiesAndConditions(GIAentityNode *
 				if(!(propertyConnection->NLCparsedForCodeBlocks))
 				{
 					#ifdef NLC_TRANSLATE_NEGATIVE_PROPERTIES_AND_CONDITIONS
-					if(propertyConnection->negative)
+					if(propertyConnection->negative || (propertyEntity->isSubstanceQuality && propertyEntity->negative))
 					{
 						#ifdef NLC_DERIVE_LOCAL_FUNCTION_ARGUMENTS_BASED_ON_IMPLICIT_DECLARATIONS
 						if(assumedToAlreadyHaveBeenDeclared(propertyEntity))

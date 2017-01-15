1edf37e94f4afb6d257b0c5763ba149855f4ca4d
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 18:12:10 +1100

    NLC 1h1a - nothing (up rev)

--- NLC/NLCtranslatorCodeBlocks.h
+++ NLC/NLCtranslatorCodeBlocks.h
@@ -26,7 +26,7 @@
  * File Name: NLCtranslatorCodeBlocks.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Natural Language Programming Interface (compiler)
- * Project Version: 1g18f 21-July-2014
+ * Project Version: 1h1a 22-July-2014
  * Requirements: requires text parsed by BAI General Intelligence Algorithm (GIA)
  *
  *******************************************************************************/

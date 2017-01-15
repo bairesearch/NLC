bcceb4da97d582778bb07681da467099d371e95d
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 18:18:38 +1100

    NLC 1u11b - no change (see GIA2n5a patch)

--- NLC/NLCitemClass.h
+++ NLC/NLCitemClass.h
@@ -26,7 +26,7 @@
  * File Name: NLCitemClass.h
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: Natural Language Compiler (Programming Interface)
- * Project Version: 1u11a 30-September-2016
+ * Project Version: 1u11b 30-September-2016
  * Requirements: requires text parsed by BAI General Intelligence Algorithm (GIA)
  *
  *******************************************************************************/

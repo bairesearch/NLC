3eeedca29ea4eb1df0625a57be39e6a6f464d854
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 18:13:21 +1100

    NLC 1j18a - [partially reverted] NLC_PREPROCESSOR_GENERATE_COMMENTS: always createCodeBlockCommentSingleLine using sentenceOriginal (not sentenceContents), even when !isMath (in the case of dummy variables)

--- NLC/NLCcodeBlockClass.cpp
+++ NLC/NLCcodeBlockClass.cpp
@@ -26,7 +26,7 @@
  * File Name: NLCcodeBlockClass.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2014 Baxter AI (baxterai.com)
  * Project: Natural Language Programming Interface (compiler)
- * Project Version: 1j17a 24-September-2014
+ * Project Version: 1j18a 24-September-2014
  * Requirements: requires text parsed by BAI General Intelligence Algorithm (GIA)
  *
  *******************************************************************************/

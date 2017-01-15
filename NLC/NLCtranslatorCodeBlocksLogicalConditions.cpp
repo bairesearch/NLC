1d6fff7e381c6e1a65a8cb45a2e99e9f66cee1ae
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 18:19:05 +1100

    NLC 1w1b - replace all NLC_DISABLE_1([a-zA-Z])_CODE_FOR_DEBUG with NLC_DEBUG_DISABLE_1\1_CODE

--- NLC/NLCtranslatorCodeBlocksLogicalConditions.cpp
+++ NLC/NLCtranslatorCodeBlocksLogicalConditions.cpp
@@ -26,7 +26,7 @@
  * File Name: NLCtranslatorCodeBlocksLogicalConditions.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2016 Baxter AI (baxterai.com)
  * Project: Natural Language Compiler (Programming Interface)
- * Project Version: 1w1a 08-December-2016
+ * Project Version: 1w1b 08-December-2016
  * Requirements: requires text parsed by BAI General Intelligence Algorithm (GIA)
  *
  *******************************************************************************/
@@ -82,7 +82,7 @@ bool generateCodeBlocksFromMathText(NLCcodeblock** currentCodeBlockInTree, map<i
 			while(stillFindingLogicalConditionAtCurrentLevel)
 			{//this loop is only required by NLC_LOGICAL_CONDITION_OPERATIONS_IF (it is not used by NLC_LOGICAL_CONDITION_OPERATIONS_WHILE and NLC_LOGICAL_CONDITION_OPERATIONS_FOR)
 
-				#ifdef NLC_DISABLE_1i_CODE_FOR_DEBUG
+				#ifdef NLC_DEBUG_DISABLE_1i_CODE
 				declareLocalPropertyListsForIndefiniteEntities(currentCodeBlockInTree, entityNodesActiveListSentence, sentenceIndex, NLCfunctionName, currentSentence);	//indefinite logical condition objects (eg "a house" in "if a house is green, do this") must have been previously declared else output code will not compile [as these statements implicitly assume the existence of "a house"; which may or may not be "green"]
 				#endif
 

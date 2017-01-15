bf250ea3729a10198e1c4e01ff9fc1cfe0ba719a
Richard Baxter <richard.baxter@baxterai.com>
2017-01-15 18:14:33 +1100

    NLC 1n5f - NLC_SUPPORT_REDEFINITIONS: update generateCodeBlocksPart5redefinitions implementation(); 6. add alsation to alsation local list (update 1n5d implementation)

--- NLC/NLCtranslatorCodeBlocks.cpp
+++ NLC/NLCtranslatorCodeBlocks.cpp
@@ -26,7 +26,7 @@
  * File Name: NLCtranslatorCodeBlocks.cpp
  * Author: Richard Bruce Baxter - Copyright (c) 2005-2015 Baxter AI (baxterai.com)
  * Project: Natural Language Programming Interface (compiler)
- * Project Version: 1n5e 17-January-2015
+ * Project Version: 1n5f 17-January-2015
  * Requirements: requires text parsed by BAI General Intelligence Algorithm (GIA)
  *
  *******************************************************************************/
@@ -742,6 +742,9 @@ bool generateCodeBlocksPart5redefinitions(NLCcodeblock ** currentCodeBlockInTree
 			{
 				*currentCodeBlockInTree =  createCodeBlockAddProperty(*currentCodeBlockInTree, parentEntity, definitionEntity, sentenceIndex);
 			}
+			
+			//6. add alsation to alsation local list
+			*currentCodeBlockInTree =  createCodeBlockAddEntityToLocalList(*currentCodeBlockInTree, definitionEntity, definitionEntity);
 			
 			*currentCodeBlockInTree = getLastCodeBlockInLevel(firstCodeBlockInLevel);
 
